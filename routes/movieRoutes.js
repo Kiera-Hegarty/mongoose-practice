9c376869033bc002ed6ab0cc64cb823d525f206f
Kiera-Hegarty <98025303+Kiera-Hegarty@users.noreply.github.com>
2022-03-17 15:58:39 +0000

    Adding working CRUD functions

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -14,14 +14,16 @@ router.get("/getAll", (req, res, next) =>{
 });
 
 router.get("/get/:id", (req, res, next) => {
-    const id = Number.parseInt(req.params.id);
+    const id = req.params.id;
+    Movie.findById(id, (err, found) => {
+    if (err)
+        return next ({status: 400, message: err.message});
+    else if (!found)
+        return next({ status: 404, message: "No movie with this id: " + id});
+    else
+        return res.send(found);
 
-    if(id == null || undefined || id === NaN)
-        return next({ status: 400, message: "Invalid id"});
-    else if (id > data.length)
-        return next({ status: 404, message: "No movie found with id" + id});
-    
-    res.json(data[id]);
+    });
 })
 
 router.post("/create", (req, res, next) => {
@@ -32,23 +34,25 @@ router.post("/create", (req, res, next) => {
 });
 
 router.put("/replace/:id", (req, res) => {
-    movie.findByIdAndUpdate(req.params.id, req.body, (err, movie) => {
-        if (err) {
-            return res
-            .send(err)
-        };
-        res.send(movie);
-    });
-    // const newMovie = req.query;
-    // const id = req.params.id;
-    // data.splice(id, 1, newMovie);
-    // res.status(202).json(data[id]);   
+    const newMovie = req.query;
+    const id = req.params.id;
+    Movie.findByIdAndUpdate(id, newMovie, (err, replaced) =>
+    {
+        if (err)
+        return next ({status: 400, message: err.message});
+        else
+        return res.status(202).send(replaced);
+    })  
 });
 
 router.delete("/remove/:id", (req, res, next) => {
-    const id = Number.parseInt(req.params.id);
-    data.splice(id, 1);
-    res.sendStatus(204);
+    const id = req.params.id;
+    Movie.findByIdAndDelete(id,(err) => {
+        if (err)
+            return next({status: 400, mesage: err.message});
+        else
+            return res.sendStatus(204);
+    })
 });
 
 module.exports = router;
